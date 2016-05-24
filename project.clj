665fdbf6bb13673a5c268b5e0b7f18682dc8e9c6
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-05-24 15:45:41 -0500

    Depend on my crajure fork

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject me.arrdem/crashpad "0.0.0"
   :source-paths ["src/main/clj"]
   :dependencies [[me.arrdem/guten-tag "0.1.6"]
-                 [crajure "0.1.1"]]
+                 [me.arrdem/crajure "0.1.2"]]
   :profiles {:dev  {:source-paths ["src/dev/clj"]}
              :jnt  {:exclusions   [org.clojure/clojure]
                     :dependencies [[org.jaunt-lang/jaunt "0.3.0-SNAPSHOT"]]}
