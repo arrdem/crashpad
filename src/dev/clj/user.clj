341af423fc0bc3cc2fa37c3ebd7601e47381e69c
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-05-24 20:01:26 -0500

    Polishing

--- src/dev/clj/user.clj
+++ src/dev/clj/user.clj
@@ -4,19 +4,22 @@
             [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
-(defn p [coll]
+(defn p [qstr coll]
   (when-not (empty? coll)
-    (println "\n* Apartments " (java.util.Date.))
+    (printf "\n* Apartments %s\n  %s\n\n"
+            (java.util.Date.) qstr)
     (doseq [{:keys [title price region url] :as e} coll]
       (printf "** %s\n   Price: $%s\n   Region: %s\n   Url: %s\n\n"
               title price region url))))
 
-(defn -main []
+(defn -main [& [?qstr]]
   (let [f       (io/file "/home/arrdem/Dropbox/crawled-apartments.org")
         g       (io/file "/home/arrdem/Dropbox/visited.edn")
+        _       (if-not (.exists g)
+                  (spit g #{}))
         visited (edn/read-string (slurp g))]
     (with-open [outf (io/writer f :append true)]
-      (let [results (->> {:query   ""
+      (let [results (->> {:query   (or ?qstr "")
                           :area    "sfbay"
                           :section :housing/apartments}
                          craj/query-cl
@@ -36,4 +39,10 @@
 
         ;; update output file
         (binding [*out* outf]
-          (p results))))))
+          (p qstr results))))))
+
+(comment
+  (-main "south of market")
+  (-main "alamo square")
+  (-main "western addition")
+  (-main "hayes valley"))
