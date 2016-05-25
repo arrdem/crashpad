6d0ab62f7159b88741eb190933a6a1db72a475b7
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-05-24 21:41:01 -0500

    Update deps

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject me.arrdem/crashpad "0.0.0"
   :source-paths ["src/main/clj"]
   :dependencies [[me.arrdem/guten-tag "0.1.6"]
-                 [me.arrdem/crajure "0.1.2"]]
+                 [me.arrdem/crajure "0.2.0"]
+                 [medley "0.8.1"]
+                 [circleci/clj-yaml "0.5.5"]]
   :profiles {:dev  {:source-paths ["src/dev/clj"]}
              :jnt  {:exclusions   [org.clojure/clojure]
                     :dependencies [[org.jaunt-lang/jaunt "0.3.0-SNAPSHOT"]]}
-             :clj  {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
+             :clj  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :user [:base :system :provided :clj]})
