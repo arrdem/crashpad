c2cccc07341a77573f2fb9c069be30181146dd7f
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-03-03 20:01:12 -0800

    Overhaul the project for Steven

--- project.clj
+++ project.clj
@@ -1,12 +1,25 @@
 (defproject me.arrdem/crashpad "0.0.0"
-  :main crashpad.core
-  :source-paths ["src/main/clj"]
-  :dependencies [[me.arrdem/guten-tag "0.1.6"]
-                 [me.arrdem/crajure "0.3.4"]
+  :description "A quick and dirty craigslist search and de-dupe tool."
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [me.arrdem/guten-tag "0.1.6"]
+                 [me.arrdem/crajure "0.4.0"]
                  [medley "0.8.1"]
                  [circleci/clj-yaml "0.5.5"]]
-  :profiles {:dev  {:source-paths ["src/dev/clj"]}
-             :jnt  {:exclusions   [org.clojure/clojure]
-                    :dependencies [[org.jaunt-lang/jaunt "0.3.0-SNAPSHOT"]]}
-             :clj  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :user [:base :system :provided :clj]})
+
+  :source-paths      ["src/main/clj"
+                      "src/main/cljc"]
+  :java-source-paths ["src/main/jvm"]
+  :test-paths        ["src/test/clj"
+                      "src/test/cljc"]
+  :resource-paths ["src/main/resources"]
+
+  :profiles {:dev {:dependencies      []
+                   :source-paths      ["src/dev/clj"
+                                       "src/dev/clj"]
+                   :java-source-paths ["src/dev/jvm"]
+                   :resource-paths    ["src/dev/resources"]}}
+
+  :main crashpad.core)
